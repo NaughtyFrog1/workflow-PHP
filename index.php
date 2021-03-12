be8474ddb4f2f9cae5dcd10705b5ae172cd926f5
Manuel Silenzi <manuelsilenzi1@gmail.com>
2021-03-11 22:09:41 -0300

    agregar index.php

---
+++ index.php
@@ -0,0 +1,11 @@
+<?php
+  declare(strict_types = 1);
+  require_once 'includes/app.php';
+  include_once 'includes/layout/header.php';
+?>
+
+<!-- CODE... -->
+
+<?php
+  include_once 'includes/layout/footer.php';
+?>
